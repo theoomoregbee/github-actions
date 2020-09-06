ec17d290ba33a747df18d8178086e7a090405bd5
Theophilus Omoregbee <theo4u@ymail.com>
2020-09-05 23:58:55 -0400

    feat: log changed files

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Done running action')
+const fileChanges = require("@trilom/file-changes-action");
+
+fileChanges().then((resp) => {
+  console.log("result of file changes", resp);
+  console.log("Done running action");
+});
